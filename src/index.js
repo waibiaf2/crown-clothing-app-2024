3273b433bd9ef085003aa448f95395af8a2b78d7
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-03-20 10:54:39 +0300

    Update various component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.scss';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import {UserProvider} from "./contexts/user.context";
+
+import {ProductsProvider} from "./contexts/products.context";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <UserProvider>
+                <ProductsProvider>
+                    <App/>
+                </ProductsProvider>
+            </UserProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
