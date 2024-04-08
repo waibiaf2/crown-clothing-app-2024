c0fe90dccc8a360ea7c37bd81142a059e2904e59
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-04-08 19:47:39 +0300

    Revert to redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 
 import {rootReducer} from "./root-reducer";
 
-const loggerMiddleWare = (store) => (next) => (action) => {
-    if(!action.type) {
-        return next(action);
-    }
-
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-    console.log('next state: ', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -26,7 +15,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleWare];
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
 
 const composedEnhancers = applyMiddleware(...middleWares);
 
